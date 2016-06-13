167d12bf3af5fbaa3d3343961ad0990ad1e1ba57
B--B <dark.schneider.ds@gmail.com>
2016-06-13 19:50:39 +0200

    mixer_paths.xml: some volume/various fixes from @arco Move audio_effects.conf to system/vendor/etc. Works in both cases, but this fix a warn during the build Fix microphones volumes Set ro.qc.sdk.audio.fluencetype=fluence (fix calls after latest changes)

--- device.mk
+++ device.mk
@@ -82,7 +82,7 @@ PRODUCT_PACKAGES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
@@ -247,7 +247,7 @@ PRODUCT_COPY_FILES += \
 # audio
 PRODUCT_PROPERTY_OVERRIDES += \
     audio.offload.disable=1 \
-    ro.qc.sdk.audio.fluencetype=none \
+    ro.qc.sdk.audio.fluencetype=fluence \
     persist.audio.fluence.voicecall=true \
     persist.audio.fluence.voicerec=false \
     persist.audio.fluence.speaker=true \
